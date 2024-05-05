8f344b544166d1964e50143517f6671e74d0c24f
Vive Velasco <48145286+DarkyRobin@users.noreply.github.com>
2024-05-05 23:00:42 +0800

    update vite and project details params

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -5,8 +5,10 @@ import { useParams } from 'react-router-dom';
 import projectsData from '../data/projects.json';
 
 interface Params {
-  id: string; // Ensure that 'id' is of type string
+  id: string;
+  [key: string]: string | undefined; // Add index signature
 }
+
 
 const ProjectDetail: React.FC = () => {
   const { id } = useParams<Params>();
